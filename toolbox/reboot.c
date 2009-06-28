b9d234104710425b5c33b87ec8daba60152d3462
Steve Kondik <shade@chemlab.org>
2009-06-28 13:41:58 -0400

    Use system services for reboot/shutdown

--- toolbox/reboot.c
+++ toolbox/reboot.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 #include <sys/reboot.h>
 #include <unistd.h>
+#include <cutils/properties.h>
 
 int reboot_main(int argc, char *argv[])
 {
     int ret;
     int nosync = 0;
     int poweroff = 0;
+    int force = 0;
 
     opterr = 0;
     do {
         int c;
 
-        c = getopt(argc, argv, "np");
+        c = getopt(argc, argv, "npf");
         
         if (c == EOF) {
             break;
@@ -27,6 +29,9 @@ int reboot_main(int argc, char *argv[])
         case 'p':
             poweroff = 1;
             break;
+    	case 'f':
+	        force = 1;
+	        break;
         case '?':
             fprintf(stderr, "usage: %s [-n] [-p] [rebootcommand]\n", argv[0]);
             exit(EXIT_FAILURE);
@@ -41,12 +46,23 @@ int reboot_main(int argc, char *argv[])
     if(!nosync)
         sync();
 
-    if(poweroff)
-        ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_POWER_OFF, NULL);
-    else if(argc > optind)
-        ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, argv[optind]);
-    else
-        ret = reboot(RB_AUTOBOOT);
+    if(force || argc > optind) {
+        if(poweroff)
+            ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_POWER_OFF, NULL);
+        else if(argc > optind)
+            ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, argv[optind]);
+        else
+            ret = reboot(RB_AUTOBOOT);
+    } else {
+        if(poweroff) {
+            property_set("ctl.start", "poweroff");
+            ret = 0;
+        } else {
+            property_set("ctl.start", "reboot");
+            ret = 0;
+        }
+    }
+
     if(ret < 0) {
         perror("reboot");
         exit(EXIT_FAILURE);
