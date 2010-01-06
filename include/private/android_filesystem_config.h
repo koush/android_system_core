8521541eba0a271dff1e7988ccb760d935fe7c4c
Steve Kondik <shade@chemlab.org>
2010-01-05 22:10:18 -0500

    Update filesystem config for init.d

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -180,6 +180,7 @@ static struct fs_path_config android_files[] = {
     { 00750, AID_ROOT,      AID_SHELL,     "sbin/*" },
     { 00755, AID_ROOT,      AID_ROOT,      "bin/*" },
     { 00750, AID_ROOT,      AID_SHELL,     "init*" },
+    { 00750, AID_ROOT,      AID_SHELL,     "system/etc/init.d/*" },
     { 00644, AID_ROOT,      AID_ROOT,       0 },
 };
 
