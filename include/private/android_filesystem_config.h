a2c18061dce3ef99f40ef79009aa097e289d40d3
Steve Kondik <shade@chemlab.org>
2010-03-14 13:29:12 -0400

    Add the sd-ext path.

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -139,6 +139,7 @@ static struct fs_path_config android_dirs[] = {
     { 00755, AID_ROOT,   AID_SHELL,  "system/xbin" },
     { 00755, AID_ROOT,   AID_ROOT,   "system/etc/ppp" },
     { 00777, AID_ROOT,   AID_ROOT,   "sdcard" },
+    { 00771, AID_SYSTEM, AID_SYSTEM, "sd-ext" },
     { 00755, AID_ROOT,   AID_ROOT,   0 },
 };
 
