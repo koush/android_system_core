7581909d4d65bd6883f77b51e600c0d42aaeafab
Steve Kondik <shade@chemlab.org>
2010-04-06 13:29:52 -0400

    Fix the build.

--- mkbootimg/Android.mk
+++ mkbootimg/Android.mk
@@ -18,18 +18,22 @@ include $(BUILD_HOST_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := mkbootimg.c
 LOCAL_STATIC_LIBRARIES := libmincrypt libcutils libc
-LOCAL_MODULE := mkbootimg
+LOCAL_MODULE := utility_mkbootimg
+LOCAL_MODULE_STEM := mkbootimg
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities/
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := unpackbootimg.c
 LOCAL_STATIC_LIBRARIES := libcutils libc
-LOCAL_MODULE := unpackbootimg
+LOCAL_MODULE := utility_unpackbootimg
+LOCAL_MODULE_STEM := unpackbootimg
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities/
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
