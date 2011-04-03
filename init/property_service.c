89255ff00b42242aef9360b8226cdde6d57d1bf0
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-04-03 19:38:18 +0100

    init: Add net device permission controls for LGE devices
    These are used by lge-ril to control the state of the mobiledata
    interfaces
    
    Change-Id: Ia869f08564c1f4907240289a32a10e0f3413d8a9a

--- init/property_service.c
+++ init/property_service.c
@@ -85,6 +85,15 @@ struct {
     { "persist.sys.",     AID_SYSTEM,   0 },
     { "persist.service.", AID_SYSTEM,   0 },
     { "persist.security.", AID_SYSTEM,   0 },
+    { "net.pdp0",         AID_RADIO,    AID_RADIO },
+    { "net.pdp1",         AID_RADIO,    AID_RADIO },
+    { "net.pdp2",         AID_RADIO,    AID_RADIO },
+    { "net.pdp3",         AID_RADIO,    AID_RADIO },
+    { "net.pdp4",         AID_RADIO,    AID_RADIO },
+    { "net.vsnet0",       AID_RADIO,    AID_RADIO },
+    { "net.vsnet1",       AID_RADIO,    AID_RADIO },
+    { "net.vsnet2",       AID_RADIO,    AID_RADIO },
+    { "net.vsnet3",       AID_RADIO,    AID_RADIO },
     { NULL, 0, 0 }
 };
 
@@ -99,6 +108,10 @@ struct {
 } control_perms[] = {
     { "dumpstate",AID_SHELL, AID_LOG },
     { "ril-daemon",AID_RADIO, AID_RADIO },
+    { "rawip_vsnet1",AID_RADIO, AID_RADIO },
+    { "rawip_vsnet2",AID_RADIO, AID_RADIO },
+    { "rawip_vsnet3",AID_RADIO, AID_RADIO },
+    { "rawip_vsnet4",AID_RADIO, AID_RADIO },
      {NULL, 0, 0 }
 };
 
