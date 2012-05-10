15be97a7986e361a52fb892b9faac9e0ed071b96
Steve Kondik <shade@chemlab.org>
2012-05-09 19:51:24 -0700

    init: Add an emmc-specific configuration file
     * Load init.emmc.rc if emmc_boot is set
     * This makes startup much cleaner for devices with a mess of vendor RC
       configurations.
    
    Change-Id: Ifa6d0ca104c11e037740690da0ff6284c2f4a3ca

--- init/init.c
+++ init/init.c
@@ -775,6 +775,12 @@ int main(int argc, char **argv)
          init_parse_config_file(tmp);
     }
 
+    /* Check for an emmc initialisation file and read if present */
+    if (emmc_boot && access("/init.emmc.rc", R_OK) == 0) {
+        INFO("Reading emmc config file");
+            init_parse_config_file("/init.emmc.rc");
+    }
+
     /* Check for a target specific initialisation file and read if present */
     if (access("/init.target.rc", R_OK) == 0) {
         INFO("Reading target specific config file");
