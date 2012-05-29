4360318c31d61e407eaf5488780f75a079545b89
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-05-29 01:10:19 +0100

    Fix Samsung chargermode logic
    Samsung devices with BOARD_CHARGING_MODE_BOOTING_LPM get their config
    out of lpm.rc. Everybody else still needs the hardware-specific init.rc
    files to read charger mode config out of.
    
    Change-Id: Ibbe0446d2b18b018258b7b2951a88014709006ac

--- init/init.c
+++ init/init.c
@@ -781,7 +781,7 @@ int main(int argc, char **argv)
     if (charging_mode_booting() || strcmp(bootmode, "charger") == 0 || strcmp(battchg_pause, BOARD_CHARGING_CMDLINE_VALUE) == 0)
         charging_mode = 1;
 
-    if (!charging_mode) {
+    if (!charging_mode_booting()) {
          snprintf(tmp, sizeof(tmp), "/init.%s.rc", hardware);
          init_parse_config_file(tmp);
 
